f8899bb7a1955dfbd162bb0a8060b108bbfbaee0
Fayth Leigh <49970213+Fayth-Leigh@users.noreply.github.com>
2019-04-25 00:44:38 -0400

    Create Admin.php

---
+++ Admin.php
@@ -0,0 +1,171 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Admin/Backend</title>
+<link href="Admin.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<header>
+<?php include 'Header.php';
+?>
+</header>
+</head>
+<body>
+    <br>
+    <br>
+<h1>Grooming Requests Admin Page</h1>
+<br>
+<p> Requests go here. Please make sure to call the clients 30 mins before their appointment. </p>
+<br>
+<table>
+<tbody><tr id="2"><td class="editable" title="GroomingID"><span contenteditable="true">2</span>
+</td><td class="editable" title="FirstName"><span contenteditable="true">Donna</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Appleseed</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 123-1234</span></td>
+<td class="editable" title="Email"><span contenteditable="true">donna@gmail.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true"><br></span></td>
+<td class="editable" title="PetName"><span contenteditable="true">Angel</span></td></tr>
+<tr id="3"><td class="editable" title="GroomingID"><span contenteditable="true">3</span>
+</td><td class="editable" title="FirstName">
+<span contenteditable="true">Bob</span></td>
+<td class="editable" title="LastName">
+<span contenteditable="true">Smith</span></td>
+<td class="editable" title="Phone">
+<span contenteditable="true">(555) 888-7777</span></td><td class="editable" title="Email">
+<span contenteditable="true">BobSmith@bob.com</span></td><td class="editable" title="PetType">
+<span contenteditable="true">dog</span></td><td class="editable" title="PetName">
+<span contenteditable="true">Stachio</span></td></tr>
+<tr id="4"><td class="editable" title="GroomingID"><span contenteditable="true">4</span>
+</td><td class="editable" title="FirstName"><span contenteditable="true">Amy</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Smith</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 444-3333</span>
+</td><td class="editable" title="Email"><span contenteditable="true">sparklesAmy@gmail.com</span>
+</td><td class="editable" title="PetType"><span contenteditable="true">cat</span></td>
+<td class="editable" title="PetName">
+<span contenteditable="true">Diamond</span></td></tr><tr id="5">
+<td class="editable" title="GroomingID"><span contenteditable="true">5</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Rupert</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Rupp</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 888-7777</span></td>
+<td class="editable" title="Email"><span contenteditable="true">RR2012@gmail.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">Fluffy</span></td></tr>
+<tr id="6"><td class="editable" title="GroomingID"><span contenteditable="true">6</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Elenor</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Reyna</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 555-3333</span></td>
+<td class="editable" title="Email"><span contenteditable="true">jj@mm.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">Loopsy</span></td></tr>
+<tr id="8"><td class="editable" title="GroomingID"><span contenteditable="true">8</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Erica</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Kudza</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">5615555823</span></td>
+<td class="editable" title="Email"><span contenteditable="true">kudza1@geewiz.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">cat</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">Marsha</span></td></tr>
+<tr id="9"><td class="editable" title="GroomingID"><span contenteditable="true">9</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Hilda</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Dynoff</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">91028388021</span></td>
+<td class="editable" title="Email"><span contenteditable="true">HildaD@flowers.com</span>
+</td><td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">Hidi</span></td></tr>
+<tr id="10"><td class="editable" title="GroomingID"><span contenteditable="true">10</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Tabes</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Phillips</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">54736635398</span></td>
+<td class="editable" title="Email"><span contenteditable="true">phillipsT@someone.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">GoldenTabs</span></td></tr>
+<tr id="11"><td class="editable" title="GroomingID"><span contenteditable="true">11</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Jepson</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 555-5550</span></td>
+<td class="editable" title="Email"><span contenteditable="true">bobj@bob.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true"></span></td>
+<td class="editable" title="PetName"><span contenteditable="true">Bosco</span></td></tr><tr id="12">
+<td class="editable" title="GroomingID"><span contenteditable="true">12</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Jepson</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 555-5550</span></td>
+<td class="editable" title="Email"><span contenteditable="true">bobj@bob.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+ <td class="editable" title="PetName"><span contenteditable="true">Bosco</span></td></tr>
+<tr id="13"><td class="editable" title="GroomingID"><span contenteditable="true">13</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Bob</span></td><td class="editable" title="LastName"><span contenteditable="true">Jepson</span></td><td class="editable" title="Phone"><span contenteditable="true">(555) 555-5550</span></td>
+<td class="editable" title="Email"><span contenteditable="true">bobj@bob.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">Bosco</span></td></tr>
+<tr id="14"><td class="editable" title="GroomingID"><span contenteditable="true">14</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Jepson</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 555-5550</span></td>
+<td class="editable" title="Email"><span contenteditable="true">bobj@bob.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">Bosco</span></td></tr>
+<tr id="15"><td class="editable" title="GroomingID"><span contenteditable="true">15</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 552-3412</span></td>
+<td class="editable" title="Email"><span contenteditable="true">fthpage@gmail.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">bbb</span></td></tr>
+<tr id="16"><td class="editable" title="GroomingID"><span contenteditable="true">16</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 552-3412</span></td>
+<td class="editable" title="Email"><span contenteditable="true">fthpage@gmail.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">bbb</span></td></tr>
+<tr id="17"><td class="editable" title="GroomingID"><span contenteditable="true">17</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 552-3412</span></td>
+<td class="editable" title="Email"><span contenteditable="true">fthpage@gmail.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">bbb</span></td></tr>
+<tr id="18"><td class="editable" title="GroomingID"><span contenteditable="true">18</span></td>
+<td class="editable" title="FirstName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="LastName"><span contenteditable="true">Bob</span></td>
+<td class="editable" title="Phone"><span contenteditable="true">(555) 552-3412</span></td>
+<td class="editable" title="Email"><span contenteditable="true">fthpage@gmail.com</span></td>
+<td class="editable" title="PetType"><span contenteditable="true">dog</span></td>
+<td class="editable" title="PetName"><span contenteditable="true">bbb</span></td></tr>
+</tbody></table>
+
+<?php
+
+$db_host = '127.0.0.1';
+$db_user = 'root';
+$db_pwd = '';
+$database = 'pet_shop';
+$table = 'grooming';
+
+@$db = new mysqli($db_host, $db_user, $db_pwd, $database);
+
+if (mysqli_connect_errno())
+{
+     echo 'Cannot connect to database: ' . mysqli_connect_error();
+}
+
+
+$query = "select * from grooming";
+
+$results = mysqli_query($db, $query);
+
+echo $results;
+         
+?>
+
+
+
+<footer>
+<?php include 'Footer.php';
+
+?>
+</footer>
+</body>
+</html>
